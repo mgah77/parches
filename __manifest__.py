b425f3bc1fc45d357d9812480de1d750779d909c
mgah77 <mauricio.gah@gmail.com>
2023-07-25 18:10:31 -0400

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -2,12 +2,12 @@
 
 {
     'name': 'Parches',
-    'version': '1.03',
+    'version': '1.04',
     'category': 'General',
     'summary': '',
     'description': """
-        - Agrega atributo de reparable a producto
-    - Menu Taller
+
+
        """,
     'author' : 'M.Gah',
     'website': '',
@@ -18,7 +18,7 @@
 	    "views/export.xml",
         "views/libro2_view.xml",
         "views/account_payment_inherit.xml",
-        "views/partner.xml"
+        "views/partner.xml",
         "security/ir.model.access.csv",
     ],
     'installable': True,
