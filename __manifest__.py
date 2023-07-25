e27513facd3e58269b75381ae0f2009c6dd8d0f5
mgah77 <mauricio.gah@gmail.com>
2023-07-25 17:28:09 -0400

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Parches',
+    'version': '1.03',
+    'category': 'General',
+    'summary': '',
+    'description': """
+        - Agrega atributo de reparable a producto
+    - Menu Taller
+       """,
+    'author' : 'M.Gah',
+    'website': '',
+    'depends': ['stock','base','sale'],
+    'data': [
+	    "security/groups.xml",
+	    "views/menu_informe.xml",
+	    "views/export.xml",
+        "views/libro2_view.xml",
+        "views/account_payment_inherit.xml",
+        "security/ir.model.access.csv",
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+}
