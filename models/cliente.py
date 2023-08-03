fe73062804dac298823ae40a9b0135793194bf71
mgah77 <mauricio.gah@gmail.com>
2023-08-03 16:20:06 -0400

    Update cliente.py

--- models/cliente.py
+++ models/cliente.py
@@ -5,7 +5,7 @@ from odoo.exceptions import ValidationError
 class ParcheCliente(models.Model):
     _inherit = 'res.partner'
 
-    property_payment_term_id = fields.Many2one(default=lambda self: self._default_plazo())
+    property_payment_term_id = fields.Many2one(required=True, default=lambda self: self._default_plazo())
 
     def _default_plazo(self):
         plazos = self.env['account.payment.term'].search([], limit=1)
