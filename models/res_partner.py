c776068600a219d902e63e2a369b708b8d1668c2
mgah77 <mauricio.gah@gmail.com>
2023-08-24 18:24:00 -0400

    Update res_partner.py

--- models/res_partner.py
+++ models/res_partner.py
@@ -4,5 +4,3 @@ from odoo.exceptions import ValidationError
 
 class ParcheCliente(models.Model):
     _inherit = 'res.partner'
-
-    company_type = fields.Selection(selection=[('company','Company'),('person','Individual')])
