97253ed30e983a882d9cd0667c12335262fb1ad3
mgah77 <138524599+mgah77@users.noreply.github.com>
2025-03-26 10:04:40 -0300

    afop

--- models/hr.py
+++ models/hr.py
@@ -10,4 +10,26 @@ class ParcheHR(models.Model):
         ('media','Media'),
         ('superior','Superior')], 'Nivel de Estudios', default='media')
 
+    afp = fields.Selection([
+        ('capital','AFP Capital'),
+        ('cuprum','AFP Cuprum'),
+        ('habitat','AFP Habitat'),
+        ('modelo','AFP Modelo'),
+        ('planvital','AFP Planvital'),
+        ('provida','AFP Provida'),
+        ('uno','AFP Uno')], 'AFP')
     
+    salud = fields.Selection([
+        ('fonasa','Fonasa'),
+        ('banmedica','Banmedica'),
+        ('colmena','Colmena'),
+        ('consalud','Consalud'),
+        ('cruzblanca','Cruz Blanca'),
+        ('masvida','Masvida'),
+        ('vidatres','Vida Tres')], 'Sistema de Salud')
+
+    caja = fields.Selection([
+        ('andes','CCAF Los Andes'),
+        ('araucana','CCAF La Araucana'),
+        ('heroes','CCAF Los Heroes'),
+        ('18','CCAF 18 de Septiembre')], 'Caja de Compensacion')
