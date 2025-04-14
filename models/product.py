e76da7a29ad7c2b5718df367ca5f91e1c76e5fba
mgah77 <138524599+mgah77@users.noreply.github.com>
2025-04-14 17:20:29 -0400

    Revert "Update product.py"
    This reverts commit a62adc692174a31bf1d203b506f5ab7b101afbbd.

--- models/product.py
+++ models/product.py
@@ -1,18 +1,11 @@
-from odoo import models, api, _
-from odoo.exceptions import UserError
+from odoo import models, api
 
 class ProductTemplate(models.Model):
     _inherit = ['product.template', 'mail.thread']
-   
- 
+    _name = 'product.template'
+    
     @api.multi
     def write(self, vals):
-        res = super(ProductTemplate, self).write(vals)
-        for record in self:
-            user_name = self.env.user.name
-            record.message_post(
-                body=_("El usuario <b>%s</b> realizó cambios en este producto.") % user_name,
-                message_type="notification",
-                subtype="mail.mt_note"
-            )
-        return res
+        message = "Producto modificado por %s" % self.env.user.name
+        self.message_post(body=message)
+        return super(ProductTemplate, self).write(vals)
