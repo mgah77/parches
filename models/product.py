cc9df342ba2276c4a7623a99c1dd769a210ee8e0
mgah77 <138524599+mgah77@users.noreply.github.com>
2025-04-14 17:51:54 -0400

    Update product.py

--- models/product.py
+++ models/product.py
@@ -1,20 +1,42 @@
 from odoo import models, api, _
+import Datetime
 import logging
+from odoo.tools.safe_eval import safe_eval
 
-_logger = logging.getLogger(__name__)
-
-class ProductTemplateInherit2(models.Model):  
+class ProductTemplate(models.Model):
     _inherit = 'product.template'
-    _name = False
+    _name = False  # necesario para que funcione la herencia sin redefinir el modelo
 
     @api.multi
     def write(self, vals):
-        _logger.info("WRITE personalizado ejecutado para product.template")
-        res = super(ProductTemplateInherit2, self).write(vals)
+        changes = {}
         for record in self:
-            record.message_post(
-                body=_("El usuario <b>%s</b> realizó cambios en este producto.") % self.env.user.name,
-                message_type="notification",
-                subtype="mail.mt_note"
-            )
+            for field, new_value in vals.items():
+                if field in record._fields:
+                    old_value = record[field]
+                    # Transforma valores complejos (many2one, etc.) a algo legible
+                    if isinstance(old_value, models.BaseModel):
+                        old_value = old_value.name
+                    if isinstance(new_value, (tuple, list)) and len(new_value) > 0:
+                        try:
+                            new_value = self.env[record._fields[field].comodel_name].browse(new_value[0]).name
+                        except:
+                            new_value = str(new_value)
+                    changes[field] = {'old': old_value, 'new': new_value}
+
+        res = super(ProductTemplate, self).write(vals)
+
+        if changes:
+            now = fields.Datetime.now().strftime('%d/%m/%Y %H:%M:%S')
+            for record in self:
+                lines = [f"<b>{field}</b>: <i>{data['old']}</i> → <i>{data['new']}</i>" for field, data in changes.items()]
+                message = _(
+                    "El usuario <b>%s</b> realizó cambios en este producto el <i>%s</i>:<br/>%s"
+                ) % (self.env.user.name, now, "<br/>".join(lines))
+
+                record.message_post(
+                    body=message,
+                    message_type="notification",
+                    subtype="mail.mt_note"
+                )
         return res
