ea9a7655309a16367a81e27a80f6a0f9461dc359
mgah77 <138524599+mgah77@users.noreply.github.com>
2025-04-14 18:24:45 -0400

    Update product.py

--- models/product.py
+++ models/product.py
@@ -1,22 +1,20 @@
 from odoo import models, api, _
-from datetime import datetime
 import logging
 
 _logger = logging.getLogger(__name__)
 
-class ProductTemplateInherit2(models.Model):  # ✅ Nombre de clase personalizado
-    _inherit = 'product.template' # ✅ Corrección válida
-
-    now = fields.datetime.now()
+class ProductTemplateInherit2(models.Model): 
+    _inherit = 'product.template'
 
     @api.multi
     def write(self, vals):
         _logger.info("WRITE personalizado ejecutado para product.template")
-        res = super(ProductTemplateInherit2, self).write(vals)        
-        formatted_time = now.strftime('%d/%m/%Y %H:%M:%S')
+        res = super(ProductTemplateInherit2, self).write(vals)
         for record in self:
+            modified_fields = ', '.join([field for field in vals.keys()])
             record.message_post(
-                body=_("El usuario <b>%s</b> realizó cambios en este producto el <i>%s</i>.") % (self.env.user.name, formatted_time),
+                body=_("El usuario <b>%s</b> realizó cambios en los siguientes campos: <b>%s</b>") % (
+                    self.env.user.name, modified_fields),
                 message_type="notification",
                 subtype="mail.mt_note"
             )
